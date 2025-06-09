ae230c84d08448f1f2b2e0379a8a9f35b3edc1a6
akSkwYX <akskwyx@gmail.com>
2025-06-09 20:49:14 +0200

    Changing color scheme from catppuccin to gruvbox

--- config/nvim/lua/plugins/color_scheme.lua
+++ config/nvim/lua/plugins/color_scheme.lua
@@ -1,11 +1,16 @@
 return {
-   'catppuccin/nvim',
+   'ellisonleao/gruvbox.nvim',
    config = function()
+      require("gruvbox").setup({
+         contrast="hard",
+      })
+
+      vim.cmd.colorscheme "gruvbox"
       local hr = tonumber(os.date('%H', os.time()))
       if hr > 6 and hr < 19 then
-         vim.cmd.colorscheme "catppuccin-latte"
+         vim.o.background "light"
       else
-         vim.cmd.colorscheme "cattpuccin-mocha"
+         vim.o.background "dark"
       end
    end
 }
