31ced8455186e3ed0ff8411942e0711e48e07fba
akSkwYX <akskwyx@gmail.com>
2025-06-10 17:06:52 +0200

    Correcting syntax error

--- config/nvim/lua/plugins/color_scheme.lua
+++ config/nvim/lua/plugins/color_scheme.lua
@@ -1,18 +1,22 @@
 return {
-   'ellisonleao/gruvbox.nvim',
-   'catppuccin/nvim',
-   config = function()
-      require("gruvbox").setup({
-         contrast="hard",
-         transparent_mode=true,
-      })
+   {
+      'ellisonleao/gruvbox.nvim',
+      config = function()
+         require("gruvbox").setup({
+            contrast="hard",
+            transparent_mode=true,
+         })
 
-      vim.cmd.colorscheme "gruvbox"
-      -- local hr = tonumber(os.date('%H', os.time()))
-      -- if hr > 6 and hr < 19 then
-      --    vim.cmd("set background=light")
-      -- else
-      --    vim.cmd("set background=dark")
-      -- end
-   end
+         vim.cmd.colorscheme "gruvbox"
+         -- local hr = tonumber(os.date('%H', os.time()))
+         -- if hr > 6 and hr < 19 then
+         --    vim.cmd("set background=light")
+         -- else
+         --    vim.cmd("set background=dark")
+         -- end
+      end
+   },
+   {
+      'catppuccin/nvim'
+   },
 }
