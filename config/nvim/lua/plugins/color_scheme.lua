00ed16456a42d3662984b876a338d6a7ee81fde7
akSkwYX <akskwyx@gmail.com>
2025-06-10 16:44:28 +0200

    Removing light theme until I find a good one (which don't burst my eye)

--- config/nvim/lua/plugins/color_scheme.lua
+++ config/nvim/lua/plugins/color_scheme.lua
@@ -1,16 +1,17 @@
 return {
    'ellisonleao/gruvbox.nvim',
+   'catppuccin/nvim',
    config = function()
       require("gruvbox").setup({
-         contrast="hard",
+         contrast="hard"
       })
 
       vim.cmd.colorscheme "gruvbox"
-      local hr = tonumber(os.date('%H', os.time()))
-      if hr > 6 and hr < 19 then
-         vim.o.background "light"
-      else
-         vim.o.background "dark"
-      end
+      -- local hr = tonumber(os.date('%H', os.time()))
+      -- if hr > 6 and hr < 19 then
+      --    vim.cmd("set background=light")
+      -- else
+      --    vim.cmd("set background=dark")
+      -- end
    end
 }
