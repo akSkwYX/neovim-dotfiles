ce241f44eb2e24c9c5d52264b60409b7130b1242
akSkwYX <akskwyx@gmail.com>
2025-06-16 12:45:59 +0200

    Add lsp for some language

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -5,9 +5,11 @@ return {
          local lspconfig = require 'lspconfig'
 
          vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-         lspconfig.lua_ls.setup {}
+         lspconfig.lua_language_server.setup {}
          lspconfig.ocamllsp.setup {}
          lspconfig.harper_ls.setup {}
+         lspconfig.java_language_server.setup {}
+         lspconfig.haskell_language_server.setup {}
       end,
    },
 }
