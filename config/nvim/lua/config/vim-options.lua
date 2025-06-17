df076495b5f70e7d25948ce8062c2d4afea7609c
akSkwYX <akskwyx@gmail.com>
2025-06-17 12:28:28 +0200

    Testing shitty command (raph)

--- config/nvim/lua/config/vim-options.lua
+++ config/nvim/lua/config/vim-options.lua
@@ -2,3 +2,5 @@ vim.cmd("set tabstop=3")
 vim.cmd("set shiftwidth=3")
 vim.cmd("set expandtab")
 vim.cmd("set relativenumber")
+
+vim.api.nvim_create_autocmd("UserGettingBored", { callback = function() vim.cmd("wqa") end, })
