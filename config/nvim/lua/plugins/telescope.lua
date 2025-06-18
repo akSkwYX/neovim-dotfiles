1aa7e6bdb4bac73991c4b1993cec2abf97d3d1bb
akSkwYX <akskwyx@gmail.com>
2025-06-18 14:19:32 +0200

    Add fzf extension for telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -20,8 +20,14 @@ return {
                ["ui-select"] = {
                   require("telescope.themes").get_dropdown {
                   }
-               }
-            }
+               },
+               fzf = {
+                  fuzzy = true,
+                  override_generic_sorter = true,
+                  override_file_sorter = true,
+                  case_mode = "smart_case",
+               },
+            },
          })
          require("telescope").load_extension("ui-select")
       end
